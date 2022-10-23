9e748fbc0cb3a4316f99358e6395cca9f39fb9a0
Kate <c.chistova2014@yandex.ru>
2022-10-23 19:25:00 +0300

    третья цифра числа

---
+++ Task13/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num > 1000)
+{
+    while(num > 1000)
+    {
+        num /= 10;
+    }
+
+    FindThirdNum(num);
+}
+else if(num > 99)
+{
+    FindThirdNum(num);
+}
+else
+{
+    Console.Write("В вашем числе нет третьей цифры");
+}
+
+void FindThirdNum(int a)
+{
+    Console.WriteLine($"Третья цифра вашего числа = {num % 10}");
+}
