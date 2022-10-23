bfac6fe542c1259c88da947aa6641555e4a9be42
Kate <c.chistova2014@yandex.ru>
2022-10-23 19:07:47 +0300

    вторая цифра числа

---
+++ Task10/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int secondNum = num / 10 % 10;
+
+Console.WriteLine($"Вторая цифра числа {num} = {secondNum}");
