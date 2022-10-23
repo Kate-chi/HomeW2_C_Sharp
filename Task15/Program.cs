514fc6b9860ba9a71422c51f8c6161e23393529e
Kate <c.chistova2014@yandex.ru>
2022-10-23 19:32:31 +0300

    поиск выходного дня

---
+++ Task15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите номер дня недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num < 6)
+{
+    Console.Write("Введенный день является рабочим :(");
+}
+else if(num > 7)
+{
+    Console.Write("Упс, вы ввели неверный день");
+}
+else
+{
+    Console.Write("Введенный день является выходным :)");
+}
